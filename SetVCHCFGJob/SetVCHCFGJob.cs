716072dd27a26b0d7919946bc5eb49fc1b338343
a991002 <a991002@ccm3s.com>
2017-03-28 09:52:06 +0800

    update:修正完取值部份

--- SetVCHCFGJob/SetVCHCFGJob.cs
+++ SetVCHCFGJob/SetVCHCFGJob.cs
@@ -7,6 +7,9 @@ using Hangfire.Server;
 
 namespace Hangfire.Topshelf.Jobs
 {
+    /// <summary>
+    /// 自動化設定單據性質設定任務.
+    /// </summary>
     public class SetVCHCFGJob : IRecurringJob
     {
         [DisplayName("自動化設定單據性質設定")]
@@ -15,16 +18,15 @@ namespace Hangfire.Topshelf.Jobs
             context.WriteLine($"{DateTime.Now:yyyy/MM/dd HH:mm:ss}設定單據性質設定任務開始執行 ...");
             // Arrange
             var connString= context.GetJobData<DBConnectionstring>("ConnectionString");
-            var jobdata = context.GetJobData();
+            var jobdata = context.GetJobData<IList<VchCfgItem>>("ITEM");
             var service = new SetVchCfgService(connString.connectionstring);
-            var field = "CLS_DT";
-            var value = "2017/03/03";
-            var vchty = "F712";
+            // 在這個任務中強制要給昨天日期
+            var yesterday = DateTime.Today.AddDays(-1).ToString("yyyy/MM/dd");
             // act
             foreach (var pair in jobdata)
             {
-                context.WriteLine($"設定單別為{vchty},將欄位({field})設定為{value}");
-                service.SetItem(field, value,vchty);
+                context.WriteLine($"設定單別為{pair.VCHTY},將欄位({pair.Field})設定為{pair.Value}");
+                service.SetItem(pair.Field,yesterday , pair.VCHTY);
             }
             var count = service.Execute();
             // Feedback

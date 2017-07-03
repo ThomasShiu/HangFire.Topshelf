2ac200ba170b77f135c50bd1123f8b9ef9751d35
goodat <thomas6712@gmail.com>
2017-07-03 08:43:34 +0800

    [更新] 離職人員系統帳號停用

--- ConfirmFormJob/Script/HR_CHGMEMBER.sql
+++ ConfirmFormJob/Script/HR_CHGMEMBER.sql
@@ -8,6 +8,7 @@ UPDATE EIP.dbo.Sys_User SET F_EnabledMark = 0 WHERE F_Account = '{1}';
 
 -- 更新ERP 帳號失效
 UPDATE [192.168.100.19].CCM_Main.dbo.USRNO SET EXP_DT = '{0:yyyy/MM/dd}' WHERE USR_NO = '{1}';
+UPDATE [192.168.100.19].CCM_Main.dbo.EMPNO SET C_INV='N', C_PUR='N', C_COP='N', C_PPS='N', C_AST='N', C_ACT='N', C_SFC='N', C_QMS='N', C_BOM='N', C_MOC ='N' WHERE EMP_NO = '{1}';
 
 -- 更新BPM 帳號失效
 UPDATE [192.168.100.18].WebBPM.dbo.FSe7en_Org_MemberStruct SET Enabled = 0 WHERE AccountID = '{1}';
